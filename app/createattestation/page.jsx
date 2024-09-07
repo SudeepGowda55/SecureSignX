9542fe7381d88a395a6af999aa1369df2b885ff3
sudeepgowda55 <ssudeepgowda55@gmail.com>
2024-09-07 19:34:15 +0000

    testng

--- app/createattestation/page.jsx
+++ app/createattestation/page.jsx
@@ -1,7 +1,8 @@
 "use client"
 
 import React, { useState } from 'react';
-import { createAttestation } from './attestation';
+// import { createAttestation } from './attestation';
+import { client } from '@/utils/signprotocol-client';
 
 const Page = () => {
 
@@ -11,6 +12,23 @@ const Page = () => {
   const [attestor, setAttestor] = useState('');
   const [submitter, setSubmitter] = useState('');
   const [complianceStatus, setComplianceStatus] = useState('');
+
+  const createAttestation = async (document_name, document_hash, ipfs_cid, attestor, submitter, compliance_status) => {
+    await client.createAttestation({
+      schemaId: "0x22c",
+      data: {
+        document_name,
+        document_hash,
+        ipfs_cid,
+        attestor,
+        submitter,
+        compliance_status
+      },
+      indexingValue: attestor.toLowerCase()
+    })
+
+    // return res
+  }
 
   const [message, setMessage] = useState('');
 
