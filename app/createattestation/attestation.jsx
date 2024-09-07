964a52ea89b1354b5c0bd4fad522ab7560e77c6c
sudeepgowda55 <ssudeepgowda55@gmail.com>
2024-09-07 19:25:35 +0000

    testing

--- app/createattestation/attestation.jsx
+++ app/createattestation/attestation.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { client } from "@/utils/signprotocol-client"
 
 export const createAttestation = async (document_name, document_hash, ipfs_cid, attestor, submitter, compliance_status) => {
