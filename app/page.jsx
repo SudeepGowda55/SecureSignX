20ecb75aa9dd087c419f92bd59064547e63c8260
SudeepGowda55 <ssudeepgowda55@gmail.com>
2024-09-08 10:17:39 +0530

    web3auth code added

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,11 @@
+"use client"
 
+import { Web3Auth } from "@web3auth/modal";
+import { useEffect, useState } from "react";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base";
+
+// import RPC from 
 export default function Home() {
   return (
     <div className="">
