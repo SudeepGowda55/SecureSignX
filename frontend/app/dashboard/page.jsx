574f9623c7224deb1e0241aa1fcfd5a607b766be
sudeepgowda55 <ssudeepgowda55@gmail.com>
2024-09-08 15:26:53 +0000

    get attestation address added

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -2,10 +2,13 @@
 
 import axios from 'axios';
 import React, { useState } from 'react';
+import { useAccount } from "wagmi"
 import { decodeAbiParameters } from 'viem';
 
 const Page = () => {
     const [attests, setAttests] = useState([]);
+
+    const { address } = useAccount();
 
     const decodeAttestation = async (attestation) => {
         const decodedAttests = [];
@@ -64,38 +67,38 @@ const Page = () => {
     return (
         <div className=" mx-auto bg-black h-screen" style={{ width: "100vw" }}>
             <h1 className="text-4xl mt-3 font-semibold text-center mb-6 text-white">GET ATTESTATIONS</h1>
-            
+
             {attests.length > 0 ? (
                 <table className="mx-2 min-w-full bg-white border border-black border-black-200">
-                <thead className="bg-gray-200">
-                    <tr>
-                        <th className="px-2 py-2 text-left border border-black">DOCUMENT Name</th>
-                        <th className="px-2 py-2 text-left border border-black">DOCUMENT Hash</th>
-                        <th className="px-2 py-2 text-left border border-black">IPFS_CID</th>
-                        <th className="px-1 py-2 text-left border border-black">ATTESTOR</th>
-                        <th className="px-1 py-2 text-left border border-black">SUBMITTER</th>
-                        <th className="px-2 py-2 text-left border border-black">Verification Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {attests.map((att, index) => (
-                        <tr key={index} className="border-t">
-                            <td className="px-2 py-2 border border-black">{att.document_name}</td>
-                            <td className="px-2 py-2 border border-black">{att.document_hash}</td>
-                            <td className="px-2 py-2 border border-black">{att.ipfs_cid}</td>
-                            <td className="px-1 py-2 border border-black">{att.attestor}</td>
-                            <td className="px-1 py-2 border border-black">{att.submitter}</td>
-                            <td className="px-2 py-2 border border-black">{att.compliance_status} </td>
+                    <thead className="bg-gray-200">
+                        <tr>
+                            <th className="px-2 py-2 text-left border border-black">DOCUMENT Name</th>
+                            <th className="px-2 py-2 text-left border border-black">DOCUMENT Hash</th>
+                            <th className="px-2 py-2 text-left border border-black">IPFS_CID</th>
+                            <th className="px-1 py-2 text-left border border-black">ATTESTOR</th>
+                            <th className="px-1 py-2 text-left border border-black">SUBMITTER</th>
+                            <th className="px-2 py-2 text-left border border-black">Verification Status</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>) : 
-            <h3 className="text-3xl mt-3 font-semibold text-center mb-6 text-white">
-                No Document Found
-            </h3>
+                    </thead>
+                    <tbody>
+                        {attests.map((att, index) => (
+                            <tr key={index} className="border-t">
+                                <td className="px-2 py-2 border border-black">{att.document_name}</td>
+                                <td className="px-2 py-2 border border-black">{att.document_hash}</td>
+                                <td className="px-2 py-2 border border-black">{att.ipfs_cid}</td>
+                                <td className="px-1 py-2 border border-black">{att.attestor}</td>
+                                <td className="px-1 py-2 border border-black">{att.submitter}</td>
+                                <td className="px-2 py-2 border border-black">{att.compliance_status} </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>) :
+                <h3 className="text-3xl mt-3 font-semibold text-center mb-6 text-white">
+                    No Document Found
+                </h3>
             }
             <button className=" mx-10 my-5 py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-             onClick={() => queryAttestations("0xC15e658AC13a89E8D2E5adBBcf29D5d168554553")}>
+                onClick={() => queryAttestations(address)}>
                 Get attestations
             </button>
         </div>
